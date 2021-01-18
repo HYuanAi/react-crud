d850f8f8eb3fcf258ffba2d20df355f22170906f
HYuanAi <yuanaiho@gmail.com>
2021-01-18 22:16:42 +0800

    Add pagination

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Card, Button, Spinner } from 'react-bootstrap';
+import { Card, Button, Spinner, Pagination } from 'react-bootstrap';
 import { ConfirmModal } from './ConfirmModal';
 
 import JobService from '../shared/job-service';
@@ -11,28 +11,59 @@ export class JobList extends React.Component {
         this.state = { 
             isLoading: false,
             showModal: false,
+            totalPages: 1,
             currentPage: 1, 
+            pageSize: 5, 
+            selectedJob: undefined,
             jobs: [] 
         };
         this.setModalShow = this.setModalShow.bind(this);
+        this.deleteJob = this.deleteJob.bind(this);
         this.loadJobs = this.loadJobs.bind(this);
+        this.setCurrentPage = this.setCurrentPage.bind(this);
+        this.getPaginationItems = this.getPaginationItems.bind(this);
     }
 
-    setModalShow(show) {
+    async setModalShow(show) {
+        if (show === false) 
+            await this.loadJobs();
         this.setState({ showModal: show});
+    }
+
+    deleteJob(job) {
+        this.setState({ selectedJob: job })
+        this.setModalShow(true);
     }
 
     async loadJobs() {
         this.isLoading = true;
 
-        const response = await JobService.retrieveJobs(this.state.currentPage, 10);
+        const response = await JobService.retrieveJobs(this.state.currentPage, this.state.pageSize);
         if (response) {
             const data = await response.json();
             this.setState({
-                isLoading: false, 
-                jobs: data
+                isLoading: false,
+                totalPages: Math.ceil(data.totalCount / this.state.pageSize),
+                jobs: data.jobs
             })
         }
+    }
+
+    async setCurrentPage(pageNumber) {
+        await this.setState({ currentPage: pageNumber });
+        this.loadJobs();
+    }
+
+    getPaginationItems(totalPages, currentPage) {
+        const paginationItems = [];
+        for (let number = 1; number <= totalPages; number++) {
+            paginationItems.push(
+                <Pagination.Item key={number} active={number === currentPage} onClick={() => this.setCurrentPage(number)}>
+                    {number}
+                </Pagination.Item>
+            );
+        }
+        return paginationItems;
     }
 
     async componentDidMount() {
@@ -40,6 +71,7 @@ export class JobList extends React.Component {
     }
 
     render() {
+        
         return (
             <>
                 { this.state.isLoading ? 
@@ -59,9 +91,9 @@ export class JobList extends React.Component {
                                         Expiry date: {job.expiry_date}
                                     </Card.Text>
                                     <LinkContainer to={`/jobs/${job.id}/edit`}>
-                                        <Button variant="primary">Edit</Button>
+                                        <Button variant="primary" className="mr-2">Edit</Button>
                                     </LinkContainer>
-                                    <Button variant="danger" onClick={() => this.setModalShow(true)}>Delete</Button>
+                                    <Button variant="danger" onClick={() => this.deleteJob(job)}>Delete</Button>
                                 </Card.Body>
                                 <Card.Footer>
                                     <small class='text-muted'>
@@ -73,11 +105,18 @@ export class JobList extends React.Component {
                         ))}
                     </>
                 </div>
+
+                <div>
+                    <Pagination size="sm" className="float-right">
+                        {this.getPaginationItems(this.state.totalPages, this.state.currentPage)}
+                    </Pagination>
+                </div>
                 
-                {/* <ConfirmModal
-                    show={this.showModal}
+                <ConfirmModal
+                    show={this.state.showModal}
                     onHide={() => this.setModalShow(false)}
-                ></ConfirmModal> */}
+                    job={this.state.selectedJob}
+                ></ConfirmModal>
             </>
         )
     }
